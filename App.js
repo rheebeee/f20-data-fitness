80fb6d85b66b6fd83083f945d564f85ff9b0a175
rheebeee <71287506+rheebeee@users.noreply.github.com>
2020-11-16 17:40:00 -0600

    added rough textbox

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, Image, View, Component, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, Image, View, Component, Button } from 'react-native';
 
 export default function App() {
   return (
@@ -9,11 +9,17 @@ export default function App() {
       <StatusBar style="auto" />
       <Button title="Login" onPress={() => alert('Hi')}/>
       <Image style={styles.logo} source={require('./assets/logo.png')} />
+
+      <TextInput
+        style={{ height: 40, borderColor: 'dodgerblue', borderWidth: 2, backgroundColor:'gainsboro', borderRadius:'10' }}
+        placeholder="email"
+        onChangeText={() => setText("Hi")}
+        
+      />
     </View>
     
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
