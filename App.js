a2f7bd0d3644029c8ee75df0b567f788cdc0a2ef
rheebeee <71287506+rheebeee@users.noreply.github.com>
2020-11-16 16:43:37 -0600

    Test

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View, Component, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Login" onPress={() => alert('Hi')}/>
     </View>
+    
   );
 }
 
@@ -16,6 +18,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center',   
   },
 });
+
+
+
+
